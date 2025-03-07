04b3e07107352227b8c23fecf2eb98969ae4f65c
Angelalpizar18 <angel_alpizar@live.com.mx>
2025-03-06 23:47:51 -0600

    PriceTag Added

--- lib/utils/donut_tile.dart
+++ lib/utils/donut_tile.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class DonutTile extends StatelessWidget {
   final String donutFlavor;
   final String donutPrice;
+  //Tipo de color
   final dynamic
       donutColor; //dynamic porque será de tipo Color y también usará []
   final String imageName;
@@ -17,9 +18,47 @@ class DonutTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
         decoration: BoxDecoration(
-      color: donutColor[50],
-    ));
+          color: donutColor[100],
+          borderRadius: BorderRadius.circular(24),
+        ),
+        child: Column(
+          children: [
+            //PriceTag
+            Row(
+              //Alinea a la derecha
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: donutColor[200],
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(24),
+                          bottomLeft: Radius.circular(24))),
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 18),
+                  child: Text('\$$donutPrice',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                          color: donutColor[800])),
+                )
+              ],
+            ),
+            //Donut Picture
+            Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 34),
+                child: Image.asset(imageName))
+            //Donut Text
+
+            //Love Icon + add Button
+          ],
+        ),
+      ),
+    );
   }
 }
