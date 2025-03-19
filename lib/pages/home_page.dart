1a61370a19227587a6fd94de6ed21c413212843b
Angelalpizar18 <angel_alpizar@live.com.mx>
2025-03-19 08:36:45 -0600

    UI finished

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -80,8 +80,43 @@ class _HomePageState extends State<HomePage> {
                 SmoothieTab(),
                 PanCakesTab(),
                 PizzaTab(),
-              ]))
+              ])),
               //Carrito
+              Container(
+                  color: Colors.white,
+                  padding: const EdgeInsets.all(16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                          padding: EdgeInsets.only(left: 28),
+                          child: Column(
+                            //Alinear a la izquierda (horizontal)
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('2 Items | \$45',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold)),
+                              Text(
+                                'Delivery Charges Included',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            ],
+                          )),
+                      ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  const Color.fromARGB(255, 218, 113, 148),
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 24, vertical: 12)),
+                          child: const Text(
+                            'View Cart',
+                            style: TextStyle(color: Colors.white),
+                          )),
+                    ],
+                  ))
             ],
           )),
     );
